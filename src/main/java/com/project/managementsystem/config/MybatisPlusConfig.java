18d41ee3c2e27d5f9a28e4028faf7fa5a5115253
MHC <3442586291@qq.com>
2023-06-15 15:25:11 +0800

    代码自动生成器

--- src/main/java/com/project/managementsystem/config/MybatisPlusConfig.java
+++ src/main/java/com/project/managementsystem/config/MybatisPlusConfig.java
@@ -1,12 +1,14 @@
+/*
 package com.project.managementsystem.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+*/
 /************************
  * Manage-System
  * com.project.managementsystem.config
@@ -14,7 +16,8 @@ import org.springframework.context.annotation.Configuration;
  * author : mhc
  * date:  2023/6/11 17:17
  * description : MybatisPlus的分页配置类
- ************************/
+ ************************//*
+
 @Configuration
 @MapperScan("com.project.managementsystem.mapper")
 public class MybatisPlusConfig {
@@ -27,3 +30,4 @@ public class MybatisPlusConfig {
     }
 
 }
+*/
